48141b4e9abf6e5a62b90f02969a5815e24b7df0
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-01-23 15:36:39 +0100

    refactor: use functions in backup and restore

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -4,12 +4,21 @@ set -o xtrace
 
 SERVER='meteorapp@coauthor.ulb.ac.be'
 CLOUD='db' # dropbox
+IDENTITY="$HOME/.ssh/meteorapp"
+
+function onserver {
+  ssh -i "$IDENTITY" "$SERVER" "$@"
+}
+
+function load {
+  rsync -e "ssh -i $IDENTITY" -a "$@"
+}
 
 cd "$(dirname "$0")"
-ssh "$SERVER" rm -rf dump/coauthor coauthor.gz || exit 1
-ssh "$SERVER" mongodump --db coauthor || exit 1
-ssh "$SERVER" tar czf coauthor.gz dump/coauthor || exit 1
-rsync -a "$SERVER":coauthor.gz coauthor-backup.gz || exit 1
+onserver rm -rf dump/coauthor coauthor.gz || exit 1
+onserver mongodump --db coauthor || exit 1
+onserver tar czf coauthor.gz dump/coauthor || exit 1
+load "$SERVER":coauthor.gz coauthor-backup.gz || exit 1
 rc copy coauthor-backup.gz "$CLOUD":coauthor-backup/"$(date '+%Y-%m-%d_%H:%M:%S')"
 
 if [ "$?" -eq 0 ] ; then
--- .backup/restore.sh
+++ .backup/restore.sh
@@ -3,12 +3,21 @@
 set -o xtrace
 
 SERVER='meteorapp@coauthor.ulb.ac.be'
+IDENTITY="$HOME/.ssh/meteorapp"
+
+function onserver {
+  ssh -i "$IDENTITY" "$SERVER" "$@"
+}
+
+function load {
+  rsync -e "ssh -i $IDENTITY" -a "$@"
+}
 
 cd "$(dirname "$0")"
-ssh "$SERVER" rm -rf dump/coauthor coauthor.gz || exit 1
-rsync -a coauthor-backup.gz "$SERVER":coauthor.gz || exit 1
-ssh "$SERVER" tar xzf coauthor.gz || exit 1
-ssh "$SERVER" mongorestore -d coauthor dump/coauthor/
+onserver rm -rf dump/coauthor coauthor.gz || exit 1
+load coauthor-backup.gz "$SERVER":coauthor.gz || exit 1
+onserver tar xzf coauthor.gz || exit 1
+onserver mongorestore -d coauthor dump/coauthor/
 
 if [ "$?" -eq 0 ] ; then
   echo 'SUCCESS!!'
